e653c1085057c6987630b60dae6143985eb8f796
Oskar <omikolaj1@gmail.com>
2019-02-18 08:23:55 -0500

    Moved the if error check into the parent component of the request handler. I am also subscribing to the responseError observable in the HTML template and creating a responseError variable which I pass onto the child component if there is an error.

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RequestError } from 'src/app/models/requesterror.model';
 import { Observable } from 'rxjs';
+import { InvokeFunctionExpr } from '@angular/compiler';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.css']
 })
-export class ErrorComponent implements OnInit {
-  @Input() error$: Observable<RequestError>;
+export class ErrorComponent implements OnInit {  
+  @Input() error: RequestError;
   constructor() { }
 
   ngOnInit() {
